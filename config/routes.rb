f67034d883dcf0dc435a71ccf97e875a6d5244a6
Ricky Gonzalez <ricky@gonzalezstrategy.com>
2022-08-08 11:19:15 -0500

    added header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :locations
   root 'locations#index'
+
+  get '/help', to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'
+  get '/about', to: 'static_pages#about'
 end
