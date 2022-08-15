394cedb95651c2d49eea3b3f93f6be0efe6f879d
Ricky Gonzalez <ricky@gonzalezstrategy.com>
2022-08-15 10:45:50 -0500

    Index cards

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,5 +1,18 @@
 module LocationsHelper
   def address location
-    "#{location.street}, #{location.city}, #{location.state}, #{location.zip}"
+    "#{location.street}, #{location.city}, #{::States.key(location.state)}, #{location.zip}"
+  end
+
+  def appointment_type(types)
+    out = "<ul>"
+    types.each do |type|
+      if type == "Walk-up"
+        out << '<li> <i class="bi bi-clock"></i> Walk-Up </li>'
+      elsif type == "Appointment Only"
+        out << '<li> <i class="bi bi-calendar-event"></i> Appointment Only </li>'
+      end
+    end
+    out << "</ul>"
+    out.html_safe
   end
 end
